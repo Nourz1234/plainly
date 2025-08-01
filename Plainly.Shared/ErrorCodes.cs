4c822e14461f3d7bf0b4b5b5bab5c8b981579147
Nourz1234 <104628709+Nourz1234@users.noreply.github.com>
2025-08-01 11:22:29 +0300

    todo

--- Plainly.Shared/ErrorCodes.cs
+++ Plainly.Shared/ErrorCodes.cs
@@ -1,7 +1,11 @@
+using System.ComponentModel;
+
 namespace Plainly.Shared;
 
 public enum ErrorCode
 {
+    // TODO: give each code a description and use error codes in response
+    // also an extension method to convert to ErrorDetail
     FullNameRequired,
     EmailRequired,
     InvalidEmail,
